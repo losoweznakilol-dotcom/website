da4342d1632e37a9773904750a0ce5fe2c4e333a
losoweznakilol-dotcom <losoweznakilol@gmail.com>
2025-10-13 14:25:15 +0200

    Add files via upload

---
+++ js.js
@@ -0,0 +1,27 @@
+
+  const burger = document.getElementById('burger');
+  const navLinks = document.getElementById('nav-links');
+
+  burger.addEventListener('click', () => {
+    burger.classList.toggle('active');
+    navLinks.classList.toggle('active');
+  });
+
+const header = document.querySelector('.tutajtest');
+const nav = document.querySelector('.nav-links');
+
+function updateMenuPosition() {
+  const headerHeight = header.offsetHeight;
+  nav.style.top = `${headerHeight}px`;
+}
+
+updateMenuPosition();
+window.addEventListener('resize', updateMenuPosition);
+
+// Jeśli header się dynamicznie zmienia np. animuje:
+const observer = new ResizeObserver(updateMenuPosition);
+observer.observe(header);
+
+function openPDF() {
+  window.open('project77_litepaper_13_10_2025.pdf', '_blank');
+}
